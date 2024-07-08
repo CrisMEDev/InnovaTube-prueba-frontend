753ca7c3d66ae24f5e6fa96bf561d399715343c7
Cristian Morales <cr15moresp@gmail.com>
2024-07-07 18:44:46 -0600

    Proyecto inicializado con las dependencias necesarias

---
+++ src/InnovaTube.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const InnovaTube = () => {
+   return (
+      <div>InnovaTube</div>
+   )
+}
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { InnovaTube } from './InnovaTube.jsx'
+
+import './normalize.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <InnovaTube />
+  </React.StrictMode>,
+)
